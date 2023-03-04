2a318bb74293321ba7c93ddfc2264f77d56783e9
Lama <lamo.boos@hotmail.com>
2023-03-04 14:24:35 +0300

    Update install-server-packages.sh

--- install-server-packages.sh
+++ install-server-packages.sh
@@ -1,10 +1,9 @@
-sudo yum update && sudo yum upgrade
-yum install git -y
-yum install wget -y
-yum install curl -y
-yum install zip -y
-yum install unzip -y
-yum install python3 -y
-yum install nano -y
-yum install tree -y
-yum install telnet -y
+git
+wget
+curl
+zip
+unzip
+python3
+nano
+tree
+telnet
